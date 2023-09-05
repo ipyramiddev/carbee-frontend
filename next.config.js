0cca2254cf701173f9128a34c5e06e01bd1b63bf
Sergio Ventura <goldendriver0731@gmail.com>
2023-09-05 05:45:31 -0600

    Setup the proxy settings in next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  async rewrites() {
+    return [
+        {
+          source: '/api/:path*',
+          destination: 'https://backend.billowing-truth-38ad.workers.dev/api/:path*'
+        }
+      ]
+  }
 }
 
 module.exports = nextConfig
